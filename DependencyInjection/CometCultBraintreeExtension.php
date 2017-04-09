5ab693deb8123bb4b5f2e5efdd4532afbab49f72
Ruslan Zavacky <ruslan.zavacky@gmail.com>
2017-04-09 18:49:25 +0100

    Use Braintree namespace

--- DependencyInjection/CometCultBraintreeExtension.php
+++ DependencyInjection/CometCultBraintreeExtension.php
@@ -6,7 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Braintree_Configuration;
+use Braintree\Configuration as Braintree_Configuration;
 
 /**
  * This is the class that loads and manages your bundle configuration
